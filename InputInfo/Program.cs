f7cf310341eff8203f20a5bc2e605b0fd3da8802
Gabriel de Morais <morais.g99@gmail.com>
2023-06-25 16:34:03 -0300

    Adicionar arquivos de projeto.

---
+++ InputInfo/Program.cs
@@ -0,0 +1,21 @@
+﻿class InputInfoApp
+{
+    static void Main()
+    {
+        Console.WriteLine("Bem-vindo ao InputInfoApp!");
+
+        Console.Write("Qual é o seu nome? ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Qual é a sua idade? ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Em qual bairro você mora? ");
+        string bairro = Console.ReadLine();
+
+        Console.WriteLine("\nInformações fornecidas:");
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Idade: " + idade);
+        Console.WriteLine("Bairro: " + bairro);
+    }
+}
